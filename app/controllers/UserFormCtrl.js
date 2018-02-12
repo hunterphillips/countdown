b7b8d0319591dfb3a734ac482b9e6c645c259cc6
Hunter Phillips <hkphillips42@Hunters-MacBook-Pro.local>
2018-02-12 13:02:55 -0600

    styles, multiple clocks, form validation

--- app/controllers/UserFormCtrl.js
+++ app/controllers/UserFormCtrl.js
@@ -2,25 +2,27 @@
 angular.module('CountdownApp')
 
    .controller('UserFormCtrl', function ($scope, TimeFactory, $interval, MetricsFactory, $timeout){
- 
    $scope.monthInput = "";
    $scope.yearInput = "";
    $scope.height = "";
    $scope.weight = "";
-   $scope.displayClock = false; 
-   $scope.endtimes = [];
-   $scope.clocks = [];
    $scope.user = {};
-   $scope.clock = {};
+   $scope.displayClock = false; 
+   $scope.clocks = [];
    let runningClock = null;
 
    $scope.removeClock = function(){
-      $scope.clock = {};
       $interval.cancel(runningClock);
+      $scope.clocks.splice(this.$index,1);
+   };
+
+   $scope.addClock = function(){
+      // $interval.cancel(runningClock);
       $scope.displayClock = false;
    };
 
    $scope.getYearsLeft = function(){
+      $scope.userForm.$valid = false;
       //format yrs/months to age for api call 
       let yrsAlive = null;
       if (+$scope.yearInput === moment().year()) {
@@ -35,21 +37,19 @@ angular.module('CountdownApp')
       $scope.user.bmi = MetricsFactory.calcBMI($scope.height, $scope.weight);
       TimeFactory.getTimeLeft($scope.user.sex, $scope.user.country, $scope.user.age, $scope.user.smokeRate, $scope.user.bmi)
       .then(date => {
+         // make new clock and add to collection
          $scope.clocks.push(TimeFactory.makeClockObj(date));
-         $scope.endtimes.push(date);
-   
-            initializeClock(date);
+         initializeClocks($scope.clocks);
       });
    };
 
-   const initializeClock = (endtime) => {
-
-      // for (let clock of $scope.clocks) {
-         runningClock = $interval(function(){
-               // $scope.clocks.splice(($scope.clocks.indexOf(clock)), 1, TimeFactory.makeClockObj(endtime));
-               $scope.clock = TimeFactory.makeClockObj(endtime);
+   const initializeClocks = (clocksArr) => {
+      // update each clock every second
+      for (let clock in clocksArr) {
+            runningClock = $interval(function(){
+               clocksArr[clock] = TimeFactory.makeClockObj(clocksArr[clock].endtime);
             }, 1000);
-         // }   
+         }
       //hide form, show clock(s)
       $scope.displayClock = true; 
    };
