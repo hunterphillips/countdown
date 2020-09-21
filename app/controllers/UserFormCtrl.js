3657acd0411d612fcdab7bec3f976159c05e6466
Hunter Phillips <hkphillips42@gmail.com>
2020-09-21 16:51:14 -0500

    manual clock

--- app/controllers/UserFormCtrl.js
+++ app/controllers/UserFormCtrl.js
@@ -1,155 +1,190 @@
-'use strict'; 
-angular.module('CountdownApp')
+"use strict";
+angular
+	.module("CountdownApp")
 
-    .controller('UserFormCtrl', function ($scope, $rootScope, TimeFactory, $interval, MetricsFactory, ChartFactory, $timeout, currentWindow){
-    $scope.monthInput = "";
-    $scope.yearInput = "";
-    $scope.height = "";
-    $scope.weight = "";
-    $scope.user = {};
-    $rootScope.displayClock = false; 
-    $scope.showYears = false;
-    $scope.clocks = [];
-    let runningClock = null;
-    
-    $scope.removeClock = function(){
-        $interval.cancel(runningClock);
-        $scope.clocks.splice(this.$index,1);
-    };
+	.controller("UserFormCtrl", function (
+		$scope,
+		$rootScope,
+		TimeFactory,
+		$interval,
+		MetricsFactory,
+		ChartFactory,
+		currentWindow
+	) {
+		$scope.monthInput = "";
+		$scope.yearInput = "";
+		$scope.height = "";
+		$scope.weight = "";
+		$scope.user = {};
+		$rootScope.displayClock = false;
+		$scope.showYears = true;
+		$scope.clocks = [];
+		// using a copy to be able to modify original array without re-rendering in ng-repeat
+		$scope.clocksCopy = [];
+		let runningClock = null;
 
-    $scope.addClock = function(){
-        // $interval.cancel(runningClock);
-        currentWindow.setSize(550, 625);
-        $rootScope.displayClock = false;
-    };
+		$scope.removeClock = function () {
+			$interval.cancel(runningClock);
+			$scope.clocks.splice(this.$index, 1);
+			$scope.clocksCopy.splice(this.$index, 1);
+		};
 
-    $scope.getYearsLeft = function(){
-        //invalidate form to disable submit btn
-        $scope.userForm.$valid = false;
-        //format yrs/months to age for api call 
-        let yrsAlive = null;
-        if (+$scope.yearInput === moment().year()) {
-            yrsAlive = 0;
-        } else {
-            yrsAlive = moment($scope.yearInput, "YYYY").fromNow().replace('a', '1').match(/\d+/)[0];
-        }
-        let monthsAlive = TimeFactory.monthsOld($scope.monthInput);
-        $scope.user.age = `${yrsAlive}y${monthsAlive}m`;
+		$scope.addClock = function () {
+			currentWindow.setSize(550, 625);
+			$rootScope.displayClock = false;
+		};
 
-        //calc bmi and fitness with weight height and exercise inputs
-        $scope.user.bmi = MetricsFactory.calcBMI($scope.height, $scope.weight);
-        $scope.user.fitLevel = MetricsFactory.calcFitness($scope.exerciseInput);
+		$scope.getYearsLeft = function () {
+			// invalidate form to disable submit btn
+			$scope.userForm.$valid = false;
+			// format yrs/months to age for api call
+			let yrsAlive = null;
+			if (+$scope.yearInput === moment().year()) {
+				yrsAlive = 0;
+			} else {
+				yrsAlive = moment($scope.yearInput, "YYYY")
+					.fromNow()
+					.replace("a", "1")
+					.match(/\d+/)[0];
+			}
+			let monthsAlive = TimeFactory.monthsOld($scope.monthInput);
+			$scope.user.age = `${yrsAlive}y${monthsAlive}m`;
 
-        TimeFactory.getTimeLeft($scope.user.sex, $scope.user.country, $scope.user.age, 
-            $scope.user.smokeRate, $scope.user.bmi, $scope.user.fitLevel)
-        .then(date => {
-            // make new clock and add to collection
-            $scope.clocks.push(TimeFactory.makeClockObj(date, $scope.user));
-            currentWindow.setSize(500,(80+(95*$scope.clocks.length)));
-            initializeClocks($scope.clocks);
-        });
-    };
+			// calc bmi and fitness with weight height and exercise inputs
+			$scope.user.bmi = MetricsFactory.calcBMI($scope.height, $scope.weight);
+			$scope.user.fitLevel = MetricsFactory.calcFitness($scope.exerciseInput);
 
-    const initializeClocks = (clocksArr) => {
-        // update each clock every second
-        for (let clock in clocksArr) {
-            runningClock = $interval(function(){
-                if($scope.showYears === true && clocksArr.length > 0){
-                    clocksArr[clock] = TimeFactory.makeYrClockObj(clocksArr[clock].endtime, clocksArr[clock]);
-                    //if time = 0, cancel interval and remove clock from array
-                    if (clocksArr[clock].total <= 0) {
-                        $interval.cancel(runningClock);
-                        clocksArr.splice(clocksArr.indexOf(clocksArr[clock]), 1);
-                    }
-                } else if(clocksArr.length > 0){
-                    clocksArr[clock] = TimeFactory.makeClockObj(clocksArr[clock].endtime, clocksArr[clock]);
-                    if (clocksArr[clock].total <= 0) {
-                        $interval.cancel(runningClock);
-                        clocksArr.splice(clocksArr.indexOf(clocksArr[clock]), 1);
-                    }
-                } 
-            }, 1000);
-        }
-        //hide form, show clock(s)
-        $rootScope.displayClock = true; 
-    };
+			TimeFactory.getTimeLeft(
+				$scope.user.sex,
+				$scope.user.country,
+				$scope.user.age,
+				$scope.user.smokeRate,
+				$scope.user.bmi,
+				$scope.user.fitLevel,
+				yrsAlive
+			).then((date) => {
+				// make new clock and add to collection
+				$scope.clocks.push(TimeFactory.makeClockObj(date, $scope.user));
+				currentWindow.setSize(500, 80 + 95 * $scope.clocks.length);
+				initializeClocks($scope.clocks);
+				$scope.clocksCopy = $scope.clocks.slice(0);
+			});
+		};
 
+		const initializeClocks = (clocksArr) => {
+			// update each clock every second
+			clocksArr.forEach(function (clock, index) {
+				runningClock = $interval(function () {
+					if (clocksArr.length > 0 && clocksArr[index]) {
+						if ($scope.showYears) {
+							clocksArr[index] = TimeFactory.makeYrClockObj(
+								clocksArr[index].endtime,
+								clocksArr[index]
+							);
+						} else {
+							clocksArr[index] = TimeFactory.makeClockObj(
+								clocksArr[index].endtime,
+								clocksArr[index]
+							);
+						}
+						if (clocksArr[index].total <= 0) {
+							$interval.cancel(runningClock);
+							clocksArr.splice(index, 1);
+							$scope.clocksCopy.splice(index, 1);
+						}
+						// copy updated values to clock's copy
+						for (let prop in clocksArr[index]) {
+							$scope.clocksCopy[index][prop] = clocksArr[index][prop];
+						}
+					}
+				}, 1000);
+			});
+			// hide form, show clock(s)
+			$rootScope.displayClock = true;
+		};
 
-    //----------------------- Chart Modal ------------//
-    $scope.toggleModal = () => {
-        // get data for chart parameters
-        $scope.labels = ChartFactory.listNames($scope.clocks);
-        $scope.data = ChartFactory.getChartData($scope.clocks);
+		//----------------------- Chart Modal ------------//
+		$scope.toggleModal = () => {
+			// get data for chart parameters
+			$scope.labels = ChartFactory.listNames($scope.clocks);
+			$scope.data = ChartFactory.getChartData($scope.clocks);
 
-        document.querySelector('.clock-modal').classList.toggle("is-active");
-        //adjust screen size for modal/clock view
-        if( document.querySelector('.clock-modal').classList.contains('is-active')){
-            currentWindow.setSize(500 + (25 * $scope.clocks.length), 585);
-        } else {
-            currentWindow.setSize(500, (80 + (95 * $scope.clocks.length)));
-        }
+			document.querySelector(".clock-modal").classList.toggle("is-active");
+			// adjust screen size for modal/clock view
+			if (
+				document.querySelector(".clock-modal").classList.contains("is-active")
+			) {
+				currentWindow.setSize(500 + 25 * $scope.clocks.length, 585);
+			} else {
+				currentWindow.setSize(500, 80 + 95 * $scope.clocks.length);
+			}
 
-        $scope.series = ['Current Years Remaining', 'Max Years Remaining'];
+			$scope.series = ["Current Years Remaining", "Max Years Remaining"];
 
-        $scope.options = { 
-            responsive: true,
-            maintainAspectRatio: false,
-            legend: { 
-                display: true,
-                labels: {
-                    fontColor: 'rgba(250,250,250,.78)',
-                    fontSize: 13,
-                    fontStyle: 'bold'
-                } 
-            },
-            scales: {
-                yAxes: [{ 
-                    display: true,
-                    ticks: { 
-                        beginAtZero: true, stepValue: 10, max: 100,
-                        fontColor: 'rgba(250,250,250,.6)',
-                    },
-                    scaleLabel: { 
-                        display: true, 
-                        labelString: 'Years', 
-                        fontSize: 15,
-                        fontColor: 'rgba(250,250,250,.78)',
-                        fontStyle: 'bold'
-                    } 
-                }],
-                xAxes: [{
-                    stacked: true,
-                    maxBarThickness: 90,
-                    ticks: {
-                        fontSize: 13.5, 
-                        fontColor: 'rgba(250,250,250,.78)',
-                        fontStyle: 'bold'
-                    },
-                }]
-            }
-        };
-    };
-    
+			$scope.options = {
+				responsive: true,
+				maintainAspectRatio: false,
 
+				legend: {
+					display: true,
+					labels: {
+						fontColor: "rgba(250,250,250,.78)",
+						fontSize: 13,
+						fontStyle: "bold",
+					},
+				},
+				scales: {
+					yAxes: [
+						{
+							display: true,
+							ticks: {
+								beginAtZero: true,
+								stepValue: 10,
+								max: 100,
+								fontColor: "rgba(250,250,250,.6)",
+							},
+							scaleLabel: {
+								display: true,
+								labelString: "Years",
+								fontSize: 15,
+								fontColor: "rgba(250,250,250,.78)",
+								fontStyle: "bold",
+							},
+						},
+					],
+					xAxes: [
+						{
+							stacked: true,
+							maxBarThickness: 90,
+							ticks: {
+								fontSize: 13.5,
+								fontColor: "rgba(250,250,250,.78)",
+								fontStyle: "bold",
+							},
+						},
+					],
+				},
+			};
+		};
 
-    //--------------------- Calender Modal ---------------//
-    $scope.toggleDate = function(){
-        // get end date and name of selected clock
-        if(this.clock){
-            $scope.graveName = this.clock.name;
-            $scope.endDate = this.clock.endtime.toDateString().substring(4);
-        }
+		//--------------------- Calender Modal ---------------//
+		$scope.toggleDate = function () {
+			// get end date and name of selected clock
+			if (this.clock) {
+				$scope.graveName = this.clock.name;
+				$scope.endDate = this.clock.endtime.toDateString().substring(4);
+			}
 
-        document.querySelector('.calender-modal').classList.toggle("is-active");
-        //adjust screen size for modal/clock view
-        if (document.querySelector('.calender-modal').classList.contains('is-active')) {
-            currentWindow.setSize(500, 500);
-        } else {
-            currentWindow.setSize(500, (80 + (95 * $scope.clocks.length)));
-        }
-    };
-
-});
-
-
-
+			document.querySelector(".calender-modal").classList.toggle("is-active");
+			//adjust screen size for modal/clock view
+			if (
+				document
+					.querySelector(".calender-modal")
+					.classList.contains("is-active")
+			) {
+				currentWindow.setSize(500, 500);
+			} else {
+				currentWindow.setSize(500, 80 + 95 * $scope.clocks.length);
+			}
+		};
+	});
--- app/factories/TimeFactory.js
+++ app/factories/TimeFactory.js
@@ -1,80 +1,94 @@
-'use strict'; 
-angular.module('CountdownApp')
+"use strict";
+angular
+	.module("CountdownApp")
 
-.factory('TimeFactory', ($q, $http) => {
+	.factory("TimeFactory", ($q, $http) => {
+		const getFutureDate = (years) => {
+			let days = years * 365;
+			//current date in milliseconds + ms til future date
+			let futureDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * days);
+			return futureDate;
+		};
 
-   const getFutureDate = (years => {
-      let days = years * 365;
-      //current date in milliseconds + ms til future date
-      let futureDate = new Date(Date.now() + (1000 * 60 * 60 * 24 * days));
-      return futureDate;
-   });
+		function makeClockObj(endtime, user) {
+			let t = Date.parse(endtime) - Date.parse(new Date());
+			let seconds = Math.floor((t / 1000) % 60);
+			let minutes = Math.floor((t / 1000 / 60) % 60);
+			let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+			let days = Math.floor(t / (1000 * 60 * 60 * 24));
 
-   function makeClockObj(endtime, user){
-      let t = Date.parse(endtime) - Date.parse(new Date());
-      let seconds = Math.floor((t / 1000) % 60);
-      let minutes = Math.floor((t / 1000 / 60) % 60);
-      let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-      let days = Math.floor(t / (1000 * 60 * 60 * 24));
+			return {
+				total: t,
+				endtime: endtime,
+				name: user.name,
+				bmi: user.bmi,
+				smokeRate: user.smokeRate,
+				fitLevel: user.fitLevel,
+				days: days,
+				hours: hours,
+				minutes: minutes,
+				seconds: seconds,
+			};
+		}
 
-      return {
-         'total': t,
-         'endtime': endtime,
-         'name': user.name,
-         'bmi': user.bmi,
-         'smokeRate': user.smokeRate,
-         'fitLevel': user.fitLevel,
-         'days': days,
-         'hours': hours,
-         'minutes': minutes,
-         'seconds': seconds
-      };
-   }
+		function makeYrClockObj(endtime, user) {
+			let t = Date.parse(endtime) - Date.parse(new Date());
+			let seconds = Math.floor((t / 1000) % 60);
+			let minutes = Math.floor((t / 1000 / 60) % 60);
+			let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+			let totalDays = Math.floor(t / (1000 * 60 * 60 * 24));
+			let years = Math.floor(totalDays / 365);
+			let days = Math.floor((totalDays / 365 - years) * 365);
+			return {
+				total: t,
+				endtime: endtime,
+				name: user.name,
+				bmi: user.bmi,
+				smokeRate: user.smokeRate,
+				fitLevel: user.fitLevel,
+				years: years,
+				days: days,
+				hours: hours,
+				minutes: minutes,
+				seconds: seconds,
+			};
+		}
 
-   function makeYrClockObj(endtime, user) {
-      let t = Date.parse(endtime) - Date.parse(new Date());
-      let seconds = Math.floor((t / 1000) % 60);
-      let minutes = Math.floor((t / 1000 / 60) % 60);
-      let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-      let totalDays = Math.floor(t / (1000 * 60 * 60 * 24));
-      let years = Math.floor(totalDays/365);
-      let days = Math.floor(((totalDays/365) - years)*365);
-      return {
-         'total': t,
-         'endtime': endtime,
-         'name': user.name,
-         'bmi': user.bmi,
-         'smokeRate': user.smokeRate,
-         'fitLevel': user.fitLevel,
-         'years': years,
-         'days': days,
-         'hours': hours,
-         'minutes': minutes,
-         'seconds': seconds
-      };
-   }
+		const monthsOld = (m) => {
+			let currentDate = new Date();
+			if (m <= currentDate.getMonth() + 1) {
+				return currentDate.getMonth() + 1 - m;
+			} else {
+				return 12 - m + (currentDate.getMonth() + 1);
+			}
+		};
 
-   const monthsOld = (m) => {
-      let currentDate = new Date();
-      if (m <= currentDate.getMonth() + 1) {
-         return (currentDate.getMonth() + 1) - m;
-      } else {
-         return (12 - m) + (currentDate.getMonth() + 1);
-      }
-   };
-
-   function getTimeLeft(sex, ctry, age, cigRate, bmi, fitness) {
-      return $q((resolve,reject) => {
-         $http.get(`http://api.population.io/1.0/life-expectancy/remaining/${sex}/${ctry}/2018-01-08/${age}`)
+		function getTimeLeft(sex, ctry, age, cigRate, bmi, fitness, yrsAlive) {
+			return $q((resolve, reject) => {
+				/*   $http.get(`http://api.population.io/1.0/life-expectancy/remaining/${sex}/${ctry}/2018-01-08/${age}`)
          .then(({ data }) => {
             // console.log('yrs left', (data.remaining_life_expectancy * cigRate * bmi * fitness));
             return (data.remaining_life_expectancy * cigRate * bmi * fitness);
          })
          .then( yrs => {
             resolve(getFutureDate(yrs));
-         });
-      });
-   }
+         });*/
 
-   return { getTimeLeft, makeClockObj, makeYrClockObj, monthsOld };
-});
+				/*
+            API WORKAROUND -- 09.09.20
+         */
+				if (yrsAlive < 75) {
+					yrsAlive = 80 - yrsAlive + yrsAlive / 15;
+				} else if (yrsAlive < 95) {
+					yrsAlive = ((110 % yrsAlive) / 6) * 2;
+				} else {
+					yrsAlive = 0.1 * (121 - yrsAlive) + 0.4;
+				}
+				let yrsRemaining = yrsAlive * sex * cigRate * bmi * fitness;
+
+				resolve(getFutureDate(yrsRemaining));
+			});
+		}
+
+		return { getTimeLeft, makeClockObj, makeYrClockObj, monthsOld };
+	});
